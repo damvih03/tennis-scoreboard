3a0bf71fcba224312e660a1c9c3288ef97f3c1e7
damvih <damvih03.official@gmail.com>
2024-09-03 00:48:43 +1000

    Refactor winPoint method

--- src/main/java/com/damvih/services/MatchScoreCalculationService.java
+++ src/main/java/com/damvih/services/MatchScoreCalculationService.java
@@ -4,29 +4,27 @@ import com.damvih.services.score.*;
 
 public class MatchScoreCalculationService {
 
-    // TODO: Refactor
     public ScoreState winPoint(FullScore fullScore, int playerNumber) {
         GameScore<?> gameScore = fullScore.getGameScore();
         SetScore setScore = fullScore.getSetScore();
         MatchScore matchScore = fullScore.getMatchScore();
 
-
-        if (gameScore.winPoint(playerNumber) != ScoreState.CONTINUE) {
-            gameScore.reset();
-
-            if (setScore.winPoint(playerNumber) == ScoreState.CONTINUE) {
-                if (setScore.isTiebreak() && gameScore instanceof RegularGameScore) {
-                    fullScore.setGameScore(new TieBreakGameScore());
-                }
-                return ScoreState.CONTINUE;
-            }
-
-            setScore.reset();
-            fullScore.setGameScore(new RegularGameScore());
-            return matchScore.winPoint(playerNumber);
-
+        if (gameScore.winPoint(playerNumber) == ScoreState.CONTINUE) {
+            return ScoreState.CONTINUE;
         }
-        return ScoreState.CONTINUE;
+        fullScore.setGameScore(new RegularGameScore());
+
+        if (setScore.winPoint(playerNumber) == ScoreState.CONTINUE) {
+
+            // Start tiebreak
+            if (setScore.isTiebreak() && gameScore instanceof RegularGameScore) {
+                fullScore.setGameScore(new TieBreakGameScore());
+            }
+            return ScoreState.CONTINUE;
+        }
+
+        setScore.reset();
+        return matchScore.winPoint(playerNumber);
     }
 
     public boolean isMatchOverAfterWinningPoint(FullScore fullScore, int playerNumber) {
